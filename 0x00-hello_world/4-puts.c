4a103f99c19cfa32efa45a4b0375220175e134a4
Its00am <butarsene@gmail.com>
2023-07-16 10:19:09 -0700

    c is fuuunnn!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * hello world C exercise
+ * main-main enrty point
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	 * return: always 0 (success)
+	 */
 	return (0);
 }
 
