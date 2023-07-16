90c91499b4daf4f0ab9d186bd9ddc4bf1e25641b
Its00am <butarsene@gmail.com>
2023-07-16 10:45:39 -0700

    description added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main-main enrty point
+ * main - enrty point
+ * Return: 0 always (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/**
-	 * return: always 0 (success)
-	 */
 	return (0);
 }
 
