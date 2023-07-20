ca1051c16606339ff39fe09a3f16ad4a2f2b4282
Its00am <butarsene@gmail.com>
2023-07-20 13:54:28 -0700

    0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _isupper - checks for upperclass letter
+ * @c: Letter to be checked
+ * Return: 1 if c is upperclass otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
